785643d1bdbeb23937a893d7d1da17ab10d80308
iekmen <irfancemekmen@icloud.com>
2026-07-23 13:36:12 +0300

    Dockerfile

---
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Eğer WordPress yüklü değilse wp-cli ile kur
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    wp core download --allow-root
+
+    wp config create \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb:3306 --allow-root
+
+    # Admin kullanıcı adı kurallara uymalıdır (admin veya administrator içeremez)
+    wp core install \
+        --url=iekmen.42.fr \
+        --title="Inception 42" \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+    wp user create \
+        $WP_NORMAL_USER $WP_NORMAL_EMAIL \
+        --role=author --user_pass=$WP_NORMAL_PASSWORD --allow-root
+fi
+
+# Dosya izinlerini düzelt
+chown -R www-data:www-data /var/www/wordpress
+
+# PHP-FPM'i ön planda çalıştır
+exec php-fpm7.4 -F
